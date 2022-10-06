a1c3d1f09d97a12ca631d5efbf9c027b4222e7c9
franconero14 <nerobalor@gmail.com>
2022-10-06 17:22:35 +0700

    Membuat class Program

--- BubbleSort1/Program.cs
+++ BubbleSort1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        // Fungsi / Method untuk menerima masukan
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data yang disimpan pada array
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if 
+            }
+        }
+    }
+}
